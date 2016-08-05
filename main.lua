910e8f2dc5051e92e8d1e1fdcd71c23e585cc597
Michael Wilson <mwilson@ponywolf.com>
2016-08-05 15:01:57 -0600

    some demo re-orging

--- main.lua
+++ main.lua
@@ -20,11 +20,17 @@ display.setDefault("background", 0.2 )
 physics.start()
 --physics.setDrawMode( "hybrid" ) 
 
-
+-- Demo 1 
 --local mapData = require "demo" -- load from lua export
+--map:extend("dragable")
+--map:extend("particle")
+
+-- Demo 2
+display.setDefault("magTextureFilter", "nearest")
+display.setDefault("minTextureFilter", "nearest")
 local mapData = json.decodeFile("demo2.json") -- load from json export
+
+-- Base loading
 local map = tiled.new(mapData)
 map.x,map.y = display.contentCenterX - map.designedWidth/2, display.contentCenterY - map.designedHeight/2
 
-map:extend("dragable")
-map:extend("particle")
